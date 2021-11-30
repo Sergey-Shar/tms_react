fad90a422dce844d9834e2cd48f11db3ef058780
Sergey-Sh <sergeihar82@gmail.com>
2021-11-30 13:45:27 +0300

    'task completed'

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Square from './components/Boxes'
+import CreateCheckbox from './components/Checkbox'
 
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Square cssClass = 'small-square' label = 'BIG square'/>
-    <Square cssClass = 'middle-square' label = 'A less square'/>
-    <Square cssClass = 'large-square' label = 'The smallest square'/>
+    <CreateCheckbox/>
   </React.StrictMode>,
   document.getElementById('root')
 );
