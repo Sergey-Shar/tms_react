cf095a4d6a23a938071bcfd6ff892ca334893647
Sergey-Sh <sergeihar82@gmail.com>
2021-11-30 14:46:56 +0300

    'task completed'

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Square from './components/Boxes'
+import Counter from './components/Counter'
 
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Square cssClass = 'small-square' label = 'BIG square'/>
-    <Square cssClass = 'middle-square' label = 'A less square'/>
-    <Square cssClass = 'large-square' label = 'The smallest square'/>
+   <Counter/>
   </React.StrictMode>,
   document.getElementById('root')
 );
