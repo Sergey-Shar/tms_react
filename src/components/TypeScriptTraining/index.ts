469973a71ac5137a1e7c70c9a217602d22e6e5ab
Sergey-Sh <sergeihar82@gmail.com>
2022-01-23 11:46:55 +0300

    'Fixed a bug in the function filterUserByAge'

--- src/components/TypeScriptTraining/index.ts
+++ src/components/TypeScriptTraining/index.ts
@@ -153,7 +153,7 @@ const countActiveUser = (users: User[]): number => {
 
 // Написать функцию которая принимает список всех пользователей и возвращает список тех, кому больше 18-ти лет
 
-const filterUserByAge = (users: User[]): object => {
+const filterUserByAge = (users: User[]): User[] => {
     return users.filter((user) => user.age > 18)
 }
 
